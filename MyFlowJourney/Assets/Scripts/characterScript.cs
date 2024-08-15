886c3c2687790e33c6165feef17dc0a24af1a886
Joseph Kimathi <joseph.kimathi@bbd.co.za>
2024-08-15 19:04:22 +0200

    quick fix for jumping bug
    @joe

--- MyFlowJourney/Assets/Scripts/characterScript.cs
+++ MyFlowJourney/Assets/Scripts/characterScript.cs
@@ -6,10 +6,12 @@ public class characterScript : MonoBehaviour
 	public Rigidbody2D Rigidbody;
 	public float VerticalSpeed = 1500;
 	public float MaxY = 3500;
+	public float MaxVerticalSpeed;
+	public float antiGravity;
 
-	public float MinX = 1500;
-	public float HorizontalSpeed = 1750;
-	public float MaxX = 3500;
+	public float MinX = 800;
+	public float HorizontalSpeed = 175;
+	public float MaxX = 1000;
 
 	public float RotationalSpeed = 325;
 
@@ -19,9 +21,10 @@ public class characterScript : MonoBehaviour
 	void Start()
 	{
 		VerticalSpeed = 1500;
-		MaxY = 3500;
+		MaxY = 1000;
+		antiGravity = 10f;
 
-		MinX = 1500;
+		MinX = 2000;
 		HorizontalSpeed = 1750;
 		MaxX = 3500;
 
@@ -29,22 +32,31 @@ public class characterScript : MonoBehaviour
 
 		IsGrounded = false;
 
-
 		Rigidbody.velocity = Vector2.right * (float)(HorizontalSpeed/100.0);
 	}
 
 	// Update is called once per frame
 	void Update()
-	{
-		if(Input.GetKeyDown(KeyCode.Space))
+  {
+    float currentDeltaTime = Time.deltaTime;
+    if (Input.GetKeyDown(KeyCode.Space))
 		{
 			if(IsGrounded)
 			{
-				var newY = Math.Min(Rigidbody.velocity.y + VerticalSpeed * Time.deltaTime, MaxY * Time.deltaTime);
-				Debug.Log($"Jumping: {newY}");
-				Debug.Log($"Pre-jump velocity: {Rigidbody.velocity.ToString("F4")}");
+				double ratio = 1 / 100.0;
+				currentDeltaTime = (float) (ratio);
+				Debug.Log(Time.deltaTime+" vs "+currentDeltaTime);
+        if (currentDeltaTime >= 1 / 50.0)
+				{
+					currentDeltaTime = (float) (ratio);
+				}
+
+				float possibleY = Rigidbody.velocity.y + 0.1f*Rigidbody.velocity.x + antiGravity * currentDeltaTime;
+				float maxY =  MaxY * currentDeltaTime;
+				Debug.Log(currentDeltaTime+" - one:"+possibleY+", two:" +maxY);
+				var newY = Math.Max(possibleY,maxY);
+
 				Rigidbody.velocity += Vector2.up * newY;
-				Debug.Log($"Postjump velocity: {Rigidbody.velocity.ToString("F4")}");
 				IsGrounded = false;
 			}
 		}
@@ -53,23 +65,22 @@ public class characterScript : MonoBehaviour
 		{
 			if(Input.GetKey(KeyCode.Space))
 			{
-				Debug.Log("Flipping");
 				Rigidbody.rotation += RotationalSpeed * Time.deltaTime;
 				// Check if we have flipped over here. If so add to a counter and then when we land add a boost
 			}
 		}
 
-		var xVelocity = Math.Min(Rigidbody.velocity.x, MaxX * Time.deltaTime);
-		xVelocity = Math.Max(xVelocity, HorizontalSpeed * Time.deltaTime);
-		//Rigidbody.velocity = Rigidbody.velocity.y * Vector2.up + xVelocity * Vector2.right;
+		var xVelocity = Math.Min(Rigidbody.velocity.x, MaxX * currentDeltaTime);
+		xVelocity = Math.Max(xVelocity, HorizontalSpeed * currentDeltaTime);
+		Rigidbody.velocity = Rigidbody.velocity.y * Vector2.up + xVelocity * Vector2.right;
 	}
 
 	public void OnCollisionEnter2D(Collision2D collision)
 	{
 		if(collision.gameObject.CompareTag("ground"))
 		{
+			Rigidbody.gravityScale = 10;
 			IsGrounded = true;
-			Debug.Log("Hit the ground!");
 		}
 
 	}
@@ -78,8 +89,8 @@ public class characterScript : MonoBehaviour
  	{
 		if(collision.gameObject.CompareTag("ground"))
 		{
+			Rigidbody.gravityScale = 1;
 			IsGrounded = false;
-			Debug.Log("Lifted from the ground!");
 		}
 	}
 }
