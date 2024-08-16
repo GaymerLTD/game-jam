534872a6ce927538a5e6b7e1de2affb4b935c805
DavidBudnitskyBBD <davidbu@bbd.co.za>
2024-08-16 18:31:28 +0200

    Added flip and boost logic

--- MyFlowJourney/Assets/Scripts/characterScript.cs
+++ MyFlowJourney/Assets/Scripts/characterScript.cs
@@ -16,7 +16,9 @@ public class characterScript : MonoBehaviour
 	public float RotationalSpeed = 325;
 
 	public bool IsGrounded = false;
-
+	public int NumFlips = 0;
+	public int MaxFlips = 10;
+	public float FlipBoost = 500;
 	// Start is called before the first frame update
 	void Start()
 	{
@@ -32,29 +34,27 @@ public class characterScript : MonoBehaviour
 
 		IsGrounded = false;
 
-		Rigidbody.velocity = Vector2.right * (float)(HorizontalSpeed/100.0);
+		Rigidbody.velocity = Vector2.right * (float)(HorizontalSpeed / 100.0);
 	}
 
 	// Update is called once per frame
 	void Update()
-  {
-    float currentDeltaTime = Time.deltaTime;
-    if (Input.GetKeyDown(KeyCode.Space))
+	{
+		float currentDeltaTime = Time.deltaTime;
+		if(Input.GetKeyDown(KeyCode.Space))
 		{
 			if(IsGrounded)
 			{
 				double ratio = 1 / 100.0;
-				currentDeltaTime = (float) (ratio);
-				Debug.Log(Time.deltaTime+" vs "+currentDeltaTime);
-        if (currentDeltaTime >= 1 / 50.0)
+				currentDeltaTime = (float)(ratio);
+				if(currentDeltaTime >= 1 / 50.0)
 				{
-					currentDeltaTime = (float) (ratio);
+					currentDeltaTime = (float)(ratio);
 				}
 
-				float possibleY = Rigidbody.velocity.y + 0.1f*Rigidbody.velocity.x + antiGravity * currentDeltaTime;
-				float maxY =  MaxY * currentDeltaTime;
-				Debug.Log(currentDeltaTime+" - one:"+possibleY+", two:" +maxY);
-				var newY = Math.Max(possibleY,maxY);
+				float possibleY = Rigidbody.velocity.y + 0.1f * Rigidbody.velocity.x + antiGravity * currentDeltaTime;
+				float maxY = MaxY * currentDeltaTime;
+				var newY = Math.Max(possibleY, maxY);
 
 				Rigidbody.velocity += Vector2.up * newY;
 				IsGrounded = false;
@@ -65,14 +65,18 @@ public class characterScript : MonoBehaviour
 		{
 			if(Input.GetKey(KeyCode.Space))
 			{
+				var prevRotation = Rigidbody.rotation;
 				Rigidbody.rotation += RotationalSpeed * Time.deltaTime;
-				// Check if we have flipped over here. If so add to a counter and then when we land add a boost
+				Rigidbody.rotation = WrapAngle(Rigidbody.rotation);
+ 				if(prevRotation < -45 && Rigidbody.rotation > -45)
+				{
+					if (NumFlips < MaxFlips) NumFlips++;
+					Debug.Log($"Flipped {NumFlips} times.");
+				}
 			}
 		}
 
-		var xVelocity = Math.Min(Rigidbody.velocity.x, MaxX * currentDeltaTime);
-		xVelocity = Math.Max(xVelocity, HorizontalSpeed * currentDeltaTime);
-		Rigidbody.velocity = Rigidbody.velocity.y * Vector2.up + xVelocity * Vector2.right;
+		BoostX(0, currentDeltaTime);
 	}
 
 	public void OnCollisionEnter2D(Collision2D collision)
@@ -81,16 +85,33 @@ public class characterScript : MonoBehaviour
 		{
 			Rigidbody.gravityScale = 10;
 			IsGrounded = true;
+			var boost = FlipBoost * NumFlips;
+			var deltaT = Time.deltaTime;
+			BoostX(boost, deltaT);
+			NumFlips = 0;
 		}
 
 	}
 
+	public void BoostX(float boost, float currentDeltaTime)
+	{
+		var xVelocity = Math.Min(Rigidbody.velocity.x + boost * currentDeltaTime, MaxX * currentDeltaTime);
+		xVelocity = Math.Max(xVelocity, HorizontalSpeed * currentDeltaTime);
+		Rigidbody.velocity = Rigidbody.velocity.y * Vector2.up + xVelocity * Vector2.right;
+	}
+
 	public void OnCollisionExit2D(Collision2D collision)
- 	{
+	{
 		if(collision.gameObject.CompareTag("ground"))
 		{
 			Rigidbody.gravityScale = 1;
 			IsGrounded = false;
 		}
+	}
+
+	private float WrapAngle(float angle)
+	{
+		var answer = ((angle + 180) % 360) - 180;
+		return answer;
 	}
 }
