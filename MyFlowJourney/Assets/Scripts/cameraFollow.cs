bdb66836ece6ab6adb795fd4e9605e7d2e87836c
Josh Jennings <josh@bbd.co.za>
2024-08-18 18:17:54 +0200

    Camera Offset

--- MyFlowJourney/Assets/Scripts/cameraFollow.cs
+++ MyFlowJourney/Assets/Scripts/cameraFollow.cs
@@ -3,7 +3,7 @@ using UnityEngine;
 public class CameraFollow : MonoBehaviour
 {
     public string playerTag = "Player"; // Tag used to find the player
-    public Vector3 offset;    // Offset from the player
+    public Vector3 offset = new Vector3(0, 0, -1);    // Offset from the player
     public float smoothSpeed = 0.125f;  // Speed at which the camera follows the player
 
     private Transform player; // Cached reference to the player's transform
